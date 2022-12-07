aa92ff9ccf7f0f21153dc464545a9f1130c9d8cd
doofu1939 <doofu1939@gmail.com>
2022-12-08 01:42:49 +0800

    Add myconfig.cfg in order to store some reused variables

--- checkWarranty.py
+++ checkWarranty.py
@@ -1,4 +1,5 @@
 import unittest
+import configparser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -11,6 +12,8 @@ class checkWarranty(unittest.TestCase):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.targetUrl = "https://www.barco.com/en/clickshare/support/warranty-info"
         self.driver.get(self.targetUrl)
+        self.config = configparser.RawConfigParser()
+        self.config.read('myConfig.cfg')
 
     def test_find_required_element(self):
         driver = self.driver
@@ -21,12 +24,13 @@ class checkWarranty(unittest.TestCase):
         get_info_btn = driver.find_element(By.XPATH, '//button[normalize-space()="Get info"]')
         driver.implicitly_wait(5)
 
-        assert intro.text == "Check your ClickShare warranty"
-        assert get_info_btn.text == "Get info"
-        assert driver.find_element(By.LINK_TEXT, "Read our warranty policy")
+        assert intro.text == self.config.get("StringValue", "INTRO_TEXT")
+        assert get_info_btn.text == self.config.get("StringValue", "BUTTON_TEXT")
+        assert driver.find_element(By.LINK_TEXT, self.config.get("StringValue", "WARRANTY_POLICY"))
 
-    def test_input_valid_serial_number_and_click(self, valid_serial ="1863552437"):
+    def test_input_valid_serial_number_and_click(self):
         driver = self.driver
+        valid_serial = self.config.get("SerialNumber", "VALID_SERIAL")
         driver.find_element(By.ID, "SerialNumber").send_keys(valid_serial)
         driver.find_element(By.XPATH, '//button[normalize-space()="Get info"]').click()
         try:
@@ -43,21 +47,22 @@ class checkWarranty(unittest.TestCase):
                 result_mapping[children[i-1].text] = children[i].text
             i+=1
 
-        assert result_mapping["Description"] == "CLICKSHARE CX-50 SET NA"
-        assert result_mapping["Part number"] == "R9861522NA"
-        assert result_mapping["Installation date"] == "09/28/2020 00:00:00"
-        assert result_mapping["Warranty end date"] == "09/27/2021 00:00:00"
-        assert result_mapping["Service contract end date"] == "01/01/0001 00:00:00"
-        assert driver.find_element(By.LINK_TEXT, "Read our warranty policy")
+        assert result_mapping[self.config.get("WarrantyInfo", "DESCRIPTION")] == "CLICKSHARE CX-50 SET NA"
+        assert result_mapping[self.config.get("WarrantyInfo", "PART_NUMBER")] == "R9861522NA"
+        assert result_mapping[self.config.get("WarrantyInfo", "INSTALLATION_DATE")] == "09/28/2020 00:00:00"
+        assert result_mapping[self.config.get("WarrantyInfo", "WARRANTY_END_DATE")] == "09/27/2021 00:00:00"
+        assert result_mapping[self.config.get("WarrantyInfo", "SERVICE_CONTRACT_END_DATE")] == "01/01/0001 00:00:00"
+        assert driver.find_element(By.LINK_TEXT, self.config.get("StringValue", "WARRANTY_POLICY"))
         
-    def test_input_too_short_serial_number_and_click(self, too_short_serial = "12345"):
+    def test_input_too_short_serial_number_and_click(self):
         driver = self.driver
+        too_short_serial = self.config.get("SerialNumber", "TOO_SHORT_SERIAL")
         driver.find_element(By.ID, "SerialNumber").send_keys(too_short_serial)
         driver.find_element(By.XPATH, '//button[normalize-space()="Get info"]').click()
         driver.implicitly_wait(5)
 
-        assert "Minimum 6 characters required" in driver.page_source
-        assert driver.find_element(By.LINK_TEXT, "Read our warranty policy")
+        assert self.config.get("StringValue", "MINIMUM_CHAR_REQUIRED") in driver.page_source
+        assert driver.find_element(By.LINK_TEXT, self.config.get("StringValue", "WARRANTY_POLICY"))
         
     def test_empty_serial_number_and_click(self, empty_input = ""):
         driver = self.driver
@@ -65,25 +70,27 @@ class checkWarranty(unittest.TestCase):
         driver.find_element(By.XPATH, '//button[normalize-space()="Get info"]').click()
         driver.implicitly_wait(5)
 
-        assert "Please specify a serial number" in driver.page_source
-        assert driver.find_element(By.LINK_TEXT, "Read our warranty policy")
+        assert self.config.get("StringValue", "EMPTY_INPUT_NOTIFICATION") in driver.page_source
+        assert driver.find_element(By.LINK_TEXT, self.config.get("StringValue", "WARRANTY_POLICY"))
         
-    def test_invalid_serial_number_and_click(self, invalid_input = "!!!!!!"):
+    def test_invalid_serial_number_and_click(self):
         driver = self.driver
+        invalid_input = self.config.get("SerialNumber", "INVALID_SERIAL_ALL_CHAR")
         driver.find_element(By.ID, "SerialNumber").send_keys(invalid_input)
         driver.find_element(By.XPATH, '//button[normalize-space()="Get info"]').click()
         driver.implicitly_wait(5)
 
-        assert "Please enter a valid serial number" in driver.page_source
-        assert driver.find_element(By.LINK_TEXT, "Read our warranty policy")
+        assert self.config.get("StringValue", "INVALID_INPUT_NOTIFICATION") in driver.page_source
+        assert driver.find_element(By.LINK_TEXT, self.config.get("StringValue", "WARRANTY_POLICY"))
             
-    def test_non_exist_serial_number_and_click(self, non_exist = "0000000000"):
+    def test_non_exist_serial_number_and_click(self):
         driver = self.driver
+        non_exist = self.config.get("SerialNumber", "NON_EXISTING_SERIAL")
         driver.find_element(By.ID, "SerialNumber").send_keys(non_exist)
         driver.find_element(By.XPATH, '//button[normalize-space()="Get info"]').click()
         driver.implicitly_wait(5)
 
-        assert driver.find_element(By.LINK_TEXT, "Check your warranty for non-ClickShare Barco Products")
+        assert driver.find_element(By.LINK_TEXT, self.config.get("StringValue", "WARRANTY_POLICY"))
             
     def tearDown(self):
         self.driver.close()
